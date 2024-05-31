3168991c1d4b263c3eb40b101bb1813c511c8ddc
Mohab Mamdouh <mohabmamdouh22@gmail.com>
2024-05-31 06:46:38 +0300

    Send to Group

--- config.php
+++ config.php
@@ -2,4 +2,5 @@
 
 $apiToken = "7148019261:AAHKochQfk4klA-mO4tG6iFcWYst8tMtyjE";
 // $chatId = "@track_items";
-$chatId = -1002213452996;
+// $chatId = -1002213452996; 
+$chatId = -1002161290109; 
--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 
+
 include('config.php');
 include('simple_html_dom.php');
 
-$notFound = 0;
+$notificationFile = 'sent_notifications.json';
 
 function sendTelegramMessage($chat_id, $message, $bot_token) {
     $url = "https://api.telegram.org/bot$bot_token/sendMessage";
@@ -39,11 +40,16 @@ function sendTelegramMessage($chat_id, $message, $bot_token) {
     }
 }
 
-// $products = ['هايلاند بيريز'];
-
-
 function checkForNewProducts() {
-    global $apiToken, $chatId, $products, $notFound;
+    global $apiToken, $chatId, $notificationFile;
+
+    // Load already sent notifications
+    if (file_exists($notificationFile)) {
+        $sentNotifications = json_decode(file_get_contents($notificationFile), true);
+    } else {
+        $sentNotifications = [];
+    }
+
     try {
         $url = "https://www.dzrt.com/ar/our-products.html";
         
@@ -53,47 +59,151 @@ function checkForNewProducts() {
         
         foreach($html->find('li.item.product.product-item') as $product) {
             if (strpos($product->class, 'unavailable') === false) {
-                $productName = $product->find('strong.product.name.product-item-name a', 0)->plaintext;
+                $productName = trim($product->find('strong.product.name.product-item-name a', 0)->plaintext);
                 $productLink = $product->find('a.product-item-link', 0)->href;
 
-                // $normalizedProductNames = array_map('trim', $products);
-
-                // if (in_array(trim($productName), $normalizedProductNames)) {
+                // Check if the product has already been notified
+                if (!in_array($productName, $sentNotifications)) {
                     $newProducts[] = [
                         'name' => $productName,
                         'link' => $productLink
                     ];
-                // }
-                $notFound = 1;
+
+                    // Add to sent notifications
+                    $sentNotifications[] = $productName;
+                }
             }
         }
         
+        // Send messages for new products
         foreach($newProducts as $newProduct) {
             $message = "منتج جديد متاح الآن: " . $newProduct['name'] . "\n" . $newProduct['link'];
             sendTelegramMessage($chatId, $message, $apiToken);
             echo $message . "<br>";
         } 
 
+        // Save sent notifications
+        file_put_contents($notificationFile, json_encode($sentNotifications));
         
     } catch (\Throwable $th) {
         throw $th;
     }
 }
 
-function notFound() {
-    global $apiToken, $chatId;
-    sendTelegramMessage($chatId, 'لا يوجد منتجات متاحة الان', $apiToken);
-}
-
-$count = 0;
-
-while(true){
-    $count = $count + 20;
+// while (true) {
     checkForNewProducts();
+    // sleep(10);
+// }
 
-    if ($notFound == 1 && $count == 60000) {
-        notFound();
-        $count = 0;
-    }
-    sleep(20);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// include('config.php');
+// include('simple_html_dom.php');
+
+// $notFound = 0;
+
+// function sendTelegramMessage($chat_id, $message, $bot_token) {
+//     $url = "https://api.telegram.org/bot$bot_token/sendMessage";
+//     $post_fields = [
+//         'chat_id' => $chat_id,
+//         'text' => $message
+//     ];
+
+//     $ch = curl_init(); 
+//     curl_setopt($ch, CURLOPT_URL, $url);
+//     curl_setopt($ch, CURLOPT_POST, true);
+//     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_fields));
+//     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+//     curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+
+//     $response = curl_exec($ch);
+//     if ($response === false) {
+//         $error = curl_error($ch);
+//         curl_close($ch);
+//         echo "CURL Error: $error";
+//         return false;
+//     }
+
+//     curl_close($ch);
+
+//     $responseData = json_decode($response, true);
+//     if (isset($responseData['ok']) && $responseData['ok']) {
+//         return true;
+//     } else {
+//         print_r($responseData);
+//         return false;
+//     }
+// }
+
+// function checkForNewProducts() {
+//     global $apiToken, $chatId, $products, $notFound;
+//     try {
+//         $url = "https://www.dzrt.com/ar/our-products.html";
+        
+//         $html = file_get_html($url);
+        
+//         $newProducts = [];
+        
+//         foreach($html->find('li.item.product.product-item') as $product) {
+//             if (strpos($product->class, 'unavailable') === false) {
+//                 $productName = $product->find('strong.product.name.product-item-name a', 0)->plaintext;
+//                 $productLink = $product->find('a.product-item-link', 0)->href;
+
+//                 $newProducts[] = [
+//                     'name' => $productName,
+//                     'link' => $productLink
+//                 ];
+
+//                 $notFound = 1;
+//             }
+//         }
+        
+//         foreach($newProducts as $newProduct) {
+//             $message = "منتج جديد متاح الآن: " . $newProduct['name'] . "\n" . $newProduct['link'];
+//             sendTelegramMessage($chatId, $message, $apiToken);
+//             echo $message . "<br>";
+//         } 
+
+        
+//     } catch (\Throwable $th) {
+//         throw $th;
+//     }
+// }
+
+// function notFound() {
+//     global $apiToken, $chatId;
+//     sendTelegramMessage($chatId, 'لا يوجد منتجات متاحة الان', $apiToken);
+//     echo 'لا يوجد منتجات متاحة الان';
+// }
+
+// while(true){
+//     checkForNewProducts();
+
+//     if ($notFound == 0) {
+//         notFound();
+//     }
+//     sleep(10);
+// }
